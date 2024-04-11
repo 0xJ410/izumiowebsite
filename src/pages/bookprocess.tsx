c90bffa1c2737a9d2b694dff8518d7232804bb31
Jhon Luke Cutay <jhonlukequibeccutay@gmail.com>
2024-04-11 23:36:32 +0800

    fixed deployment failing

--- src/pages/bookprocess.tsx
+++ src/pages/bookprocess.tsx
@@ -1,5 +1,4 @@
 import BookingProcessComponent from "../component/BookingProcess";
-import Footer from "../component/Footer";
 import { NavigationBarBookingProcess } from "../component/NavigationBar";
 
 export default function BookingProcess() {
